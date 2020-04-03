361490e09ca43010fcdda8aa6242c6f5197ceeb0
Shreyas Sood <shreyassood@gmail.com>
2020-04-03 02:20:49 -0500

    Simplify app

--- src/main/java/edu/utexas/cs/cs312/StyleCheckerApplication.java
+++ src/main/java/edu/utexas/cs/cs312/StyleCheckerApplication.java
@@ -2,12 +2,22 @@ package edu.utexas.cs.cs312;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class StyleCheckerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StyleCheckerApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
 	}
 
 }
