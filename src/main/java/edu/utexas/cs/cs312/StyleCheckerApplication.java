0c2e6180683b9a5828187b63ab92c2a4fe8bfa52
Shreyas Sood <shreyassood@gmail.com>
2020-04-03 01:43:08 -0500

    Basic java spring template

---
+++ src/main/java/edu/utexas/cs/cs312/StyleCheckerApplication.java
@@ -0,0 +1,13 @@
+package edu.utexas.cs.cs312;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utexas/cs/cs312/StyleCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utexas.cs.cs312;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
