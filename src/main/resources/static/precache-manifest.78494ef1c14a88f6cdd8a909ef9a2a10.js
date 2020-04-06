d4fdd773766ca4fc4f4b96b1d5de1b8fc494577e
Shreyas Sood <shreyassood@gmail.com>
2020-04-06 02:57:53 -0500

    Add basic static resources from react

---
+++ src/main/resources/static/precache-manifest.78494ef1c14a88f6cdd8a909ef9a2a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feeac97a11258c8654897bca6a22a5d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40ebbca20cfe97865295",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d15b677743413b16879d",
+    "url": "/static/js/2.e4e8c54e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4e8c54e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ebbca20cfe97865295",
+    "url": "/static/js/main.b2e7341f.chunk.js"
+  },
+  {
+    "revision": "4cdd0c0fafa1c0a3a28c",
+    "url": "/static/js/runtime-main.314b3586.js"
+  }
+]);
