eb542e6859c3e21d8605bfdb3952d82a0527dda7
Shreyas Sood <shreyassood@gmail.com>
2020-04-08 15:59:18 -0500

    Fail mvn script on react build fail

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-run () {
-    (cd web && npm install && npm run build)
-    rm -rf ./src/main/resources/static
-    cp -r web/build ./src/main/resources/static
-}
-
-run
+(cd web &&
+	npm install &&
+	npm run build &&
+	cd .. &&
+	rm -rf ./src/main/resources/static &&
+	cp -r web/build ./src/main/resources/static)
