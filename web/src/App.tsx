b0867e5eba05601c4b614ad99042e40b37146d94
Shreyas Sood <shreyassood@gmail.com>
2020-04-19 22:39:07 -0500

    Change app to class component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,15 +2,33 @@ import React from 'react';
 import HelloWorld from './components/HelloWorld'
 import FileDrop from './components/FileDrop'
 
-export default function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <HelloWorld/>
-            </header>
+type Props = {}
 
-            <FileDrop/>
+type State = {
+    uploadedDocument: boolean
+}
+
+export default class App extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            uploadedDocument: false,
+        }
+    }
 
 
-        </div>);
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <HelloWorld/>
+                </header>
+
+                <FileDrop/>
+
+            </div>
+        );
+    }
+
 }
